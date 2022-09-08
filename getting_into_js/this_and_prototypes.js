3bc10299606a4961755de385d67266e0b6ac4970
Njeri Cooper <njeri.cooper@youthvillages.org>
2022-09-08 18:45:42 -0400

    prototypes

--- getting_into_js/this_and_prototypes.js
+++ getting_into_js/this_and_prototypes.js
@@ -20,3 +20,36 @@ function otherClass() {
 }
 
 otherClass();
+
+//constructing
+
+function Workshop(teacher) {
+    this.teacher = teacher;
+}
+Workshop.prototype.ask = function(question) {
+    console.log(this.teacher, question);
+};
+
+var deepJS = new Workshop("Kyle");
+var reactJS = new Workshop("Suzy");
+
+deepJS.ask("Is 'prototype' a class?");
+//Kyle isn't prototype a class?
+reactJS.ask("isn't 'prototype' ugly?");
+//Suzy isn't prototype ugly
+
+class Workshop {
+    constructor(teacher) {
+        this.teacher = teacher;
+    }
+    ask(question) {
+        console.log(this.teacher , question);
+    }
+}
+var deepJS = new Workshop("Kyle");
+var reactJS = new Workshop("Suzy");
+
+deepJS.ask("Is 'class' a class?");
+//Kyle is 'class'  a class?
+reactJS.ask("Is this class OK?");
+//Suzy is this class OK? 
